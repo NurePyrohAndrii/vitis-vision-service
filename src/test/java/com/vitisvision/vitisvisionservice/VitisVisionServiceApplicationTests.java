2f22a60ab906a3d909ba99d613885bcd58da034d
Andrii Pyroh <andrewpyrih@gmail.com>
2024-03-03 15:15:23 +0200

    Performed basic project set up

---
+++ src/test/java/com/vitisvision/vitisvisionservice/VitisVisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitisvision.vitisvisionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitisVisionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
